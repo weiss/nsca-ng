b88217656355e21a45a80cf30c18c4f333354111
Holger Weiß <holger@weiss.in-berlin.de>
2013-02-11 00:20:28 +0100

    Cosmetic change: replace 0 with '\0'
    A '\0' should be assigned to nul-terminate a string.

--- src/common/util.c
+++ src/common/util.c
@@ -62,7 +62,7 @@ concat(const char *str1, const char *str2)
 	catenated = xmalloc(size);
 	(void)memcpy(catenated, str1, len1);
 	(void)memcpy(catenated + len1, str2, len2);
-	catenated[len1 + len2] = 0;
+	catenated[len1 + len2] = '\0';
 
 	return catenated;
 }
